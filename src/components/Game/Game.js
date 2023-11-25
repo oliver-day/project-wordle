bf468f526ba9415a82d8aba230c52ef8f50a0175
Oliver Day <oliver.day.swe@gmail.com>
2023-11-25 15:05:01 -0800

    feat: complete 'Word Game' exercise

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,14 +2,66 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { checkGuess } from '../../game-helpers';
 
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+import GuessInput from '../GuessInput';
+import GuessResults from '../GuessResults';
+import WonBanner from '../WonBanner';
+import LostBanner from '../LostBanner';
+import Keyboard from '../Keyboard';
 
 function Game() {
-  return <>Put a game here!</>;
+  const [answer, setAnswer] = React.useState(() => sample(WORDS));
+
+  // running | won | lost
+  const [gameStatus, setGameStatus] = React.useState('running');
+
+  const [guesses, setGuesses] = React.useState([]);
+
+  function handleSubmitGuess(tentativeGuess) {
+    const nextGuesses = [...guesses, tentativeGuess];
+    setGuesses(nextGuesses);
+
+    if (tentativeGuess.toUpperCase() === answer) {
+      setGameStatus('won');
+    } else if (nextGuesses.length >= NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus('lost');
+    }
+  }
+
+  function handleRestart() {
+    const newAnswer = sample(WORDS);
+    setAnswer(newAnswer);
+    setGuesses([]);
+    setGameStatus('running');
+  }
+
+  const validatedGuesses = guesses.map((guess) => checkGuess(guess, answer));
+
+  return (
+    <>
+      <GuessResults validatedGuesses={validatedGuesses} />
+      <GuessInput
+        gameStatus={gameStatus}
+        handleSubmitGuess={handleSubmitGuess}
+      />
+      <Keyboard validatedGuesses={validatedGuesses} />
+
+      {gameStatus === 'won' && (
+        <WonBanner
+          numOfGuesses={guesses.length}
+          handleRestart={handleRestart}
+        />
+      )}
+      {gameStatus === 'lost' && (
+        <LostBanner
+          answer={answer}
+          handleRestart={handleRestart}
+        />
+      )}
+    </>
+  );
 }
 
 export default Game;
---
+++ src/components/GuessResults/GuessResults.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import { range } from '../../utils';
+
+import Guess from '../Guess';
+
+function GuessResults({ validatedGuesses }) {
+  return (
+    <div className="guess-results">
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess
+          key={num}
+          value={validatedGuesses[num]}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default GuessResults;
